4c56ec733c55366f435dc45912451b0af3a916e3
rlibowsky <libowsky@usc.edu>
2018-02-16 11:31:14 -0800

    signup fixed and routed to portal page

--- logo-detection-frontend/src/components/signup.js
+++ logo-detection-frontend/src/components/signup.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Container, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-export default class SignUp extends React.Component { 
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(ev) {
+    this.props.history.push('/portal');
+    /* TODO: validate the data */
+  }
 
   render() {
     return (
@@ -21,10 +30,10 @@ export default class SignUp extends React.Component {
           <Label for="password">Retype Password</Label>
           <Input type="password" name="passwordVerify" id="passwordVerify" placeholder="Retype Password" />
         </FormGroup>
-        <Button>Go</Button>
+        <Button onClick={this.onSubmit}> Go </Button>
         <FormGroup>
             <div> Already have an account? </div>
-            <a href="/signup"> Sign up </a>
+            <a href="/login"> Log In </a>
         </FormGroup>
       </Form>
       </center>
