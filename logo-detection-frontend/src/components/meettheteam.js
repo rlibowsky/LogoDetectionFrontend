dea0ca23772dfe04beec45e737a495850f5ad3f5
Shabina Rayan <srayan@usc.edu>
2018-02-16 22:37:25 -0800

    some cha cha cha cha changes

--- logo-detection-frontend/src/components/meettheteam.js
+++ logo-detection-frontend/src/components/meettheteam.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './meettheteam.css';
-
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+import ReactDOM from 'react-dom';
+var Carousel = require('nuka-carousel');
 
 export default class MeetTheTeam extends React.Component {
   constructor(props) {
     super(props);
-
     this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false
@@ -30,27 +18,25 @@ export default class MeetTheTeam extends React.Component {
   }
   render() {
     return (
-      <div>
-
-        <div class="image" >
-          <img src={require('./images/meettheteam/anubhav.jpg')} width="400" height="400" hspace = "50" vspace = "10"/>
-          <h2><span>Anubhav Sarkar:<span class='spacer'></span><br /><span class='spacer'></span>Full Stack Developer</span></h2>
-        </div>
-
-        <div class="image">
-          <img src={require('./images/meettheteam/ruth.jpg')} width="400" height="400" hspace = "50" vspace = "10" />
-          <h2><span>Ruth Libowsky:<span class='spacer'></span><br /><span class='spacer'></span>Front-End Developer</span></h2>
-        </div>
-
-        <div class="image">
-          <img src={require('./images/meettheteam/kevin.jpg')} width="400" height="400" hspace = "50" vspace = "10" />
-          <h2><span>Kevin Wang:<span class='spacer'></span><br /><span class='spacer'></span>Back-End Developer</span></h2>
-        </div>
-
-        <div class="image">
-          <img src={require('./images/meettheteam/shabina.jpg')} width="400" height="400"  hspace = "50" vspace = "10" />
-          <h2><span>Shabina Rayan:<span class='spacer'></span><br /><span class='spacer'></span>Front-End Developer</span></h2>
-        </div>
+      <div align = "center">
+      <Carousel width="40%">
+      <figure class="wp-caption">
+        <img class="demo" src={require('./images/meettheteam/anubhav.jpg')} alt="Image" />
+        <figcaption class="wp-caption-text">Anubhav Sarkar: Full Stack Developer</figcaption>
+      </figure>
+      <figure class="wp-caption">
+        <img class="demo" src={require('./images/meettheteam/shabina.jpg')} alt="Image" />
+        <figcaption class="wp-caption-text">Shabina Rayan: Frontend Developer</figcaption>
+      </figure>
+      <figure class="wp-caption">
+        <img class="demo" src={require('./images/meettheteam/ruth.jpg')} alt="Image" />
+        <figcaption class="wp-caption-text">Ruth Libowsky: Frontend Developer</figcaption>
+      </figure>
+      <figure class="wp-caption">
+        <img class="demo" src={require('./images/meettheteam/kevin.jpg')} alt="Image" />
+        <figcaption class="wp-caption-text">Kevin Wang: Backend Developer</figcaption>
+      </figure>
+      </Carousel>
       </div>
     );
   }
--- logo-detection-frontend/src/components/portal.js
+++ logo-detection-frontend/src/components/portal.js
@@ -1,16 +1,61 @@
 import React from 'react';
-import { Container, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './portal.css';
 
-export default class Portal extends React.Component { 
+import {
+  Container,
+  Col,
+  Button,
+  Form,
+  FormGroup, 
+  Label, 
+  Input, 
+  FormText,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
+  export default class Portal extends React.Component { 
   render() {
     return (
     <Container>
         <center>
             <h2> OUR SERVICES </h2>
             <h5> Here for you </h5>
-      </center>
-    </Container>
+            <div class="bt-space"></div>
+
+            <div class="row">
+              
+              <div class="column">
+                <div class="row">
+                  <img class="demo" src={require('./images/services/train.jpg')} alt="Image" height="300" width="400"/>
+                </div>
+                <div class="row">
+                  <Button onClick={this.onSubmit}> Go </Button>
+                </div>
+              </div>
+
+              <div class="bt-space"></div>
+
+              <div class="column">
+                <div class="row">
+                  <img class="demo" src={require('./images/services/search.jpg')} alt="Image" height="300" width="400"/>
+                </div>
+                <div class="row">
+                  <Button onClick={this.onSubmit}> Go </Button>
+                </div>
+              </div>
+
+          </div>
+        </center>
+    </Container>  
     );
   }
 }
