1b99216aba734d5d5b85e311a3e8b69610c327c7
Shabina Rayan <srayan@usc.edu>
2018-02-20 17:15:31 -0800

    login authentification added

--- logo-detection-frontend/src/components/login.js
+++ logo-detection-frontend/src/components/login.js
@@ -4,30 +4,51 @@ import { Container,  Button, Form, FormGroup,  Input } from 'reactstrap';
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      data: '',
+      wrongLogin: false
+    }
   }
 
-  onSubmit(ev) {
-    this.props.history.push('/portal');
-    /* TODO: validate the data */
-    console.log("here")
-    fetch('http://localhost:2000/users/signup/', {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            "email": "testing@gmail.com",
-            "password": "secret",
-            "name": "matthew",
-            "organization": "Apple"
-          })
-    }).then(function(response) {
-     return response.json();
-    }).then(function(data) {
-      console.log(data)
-     });
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.email)
+    console.log(this.state.password)
+    fetch('http://localhost:2000/users/login/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "email": this.state.email,
+        "password": this.state.password,
+      })
+    }).then(response => {
+      console.log(response.status)
+      if (response.status == 200) {
+        this.state.wrongLogin = false;
+        this.props.history.push('/portal');
+      }
+      else {
+        this.state.wrongLogin = true;
+      }
+    }).then(data => {
+      this.setState({data: data })
+  });  
   }
 
   render() {
@@ -37,11 +58,33 @@ export default class Login extends React.Component {
             <h1> Login </h1>
     <Form>
         <FormGroup>
-          <Input type="email" name="email" placeholder="Email" />
+          <Input 
+            type="email" 
+            name="email" 
+            placeholder="Email"  
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
         </FormGroup>
         <FormGroup>
-          <Input type="password" name="password" id="password" placeholder="Password" />
+          <Input 
+            type="password" 
+            name="password" 
+            id="password" 
+            placeholder="Password" 
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
         </FormGroup>
+
+        <div> 
+              {this.state.wrongLogin ? (
+              <h5> incorrect password </h5>
+            ) : (
+              <h5> </h5>
+            )}
+        </div>
+
         <FormGroup>
             <div class="switchCheckBox"> 
               <Input addon type="checkbox"/> 
@@ -49,7 +92,7 @@ export default class Login extends React.Component {
             </div>
             <a class="switchLinkRight" href="/"> Forgot Password? </a>
         </FormGroup>
-        <Button onClick={this.onSubmit} >Go</Button>
+        <Button onClick={this.handleSubmit} >Go</Button>
         <FormGroup>
             <div class="switchText"> Don't have an account? </div>
             <a class="switchLink" href="/signup"> Sign up </a>
