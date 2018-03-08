ec8c23efe12548ed18aeb5795a00528a4fa29550
Shabina Rayan <srayan@usc.edu>
2018-03-07 16:17:38 -0800

    border when clicked

--- logo-detection-frontend/src/components/scrapeInstagram.js
+++ logo-detection-frontend/src/components/scrapeInstagram.js
@@ -19,29 +19,48 @@ const CSSVariables = {
       super(props);
       this.state = {
         token: cookie.load('token'),
-        selectedImages: [],
-        showBorder : false
+        showBorder : false,
+        lastClicked: ''
       };
       if (this.state.token === undefined) {
         this.props.history.push('/login');
         return;
       }
+      this.selectedImages = [];
       this.searchTerms = props.location.params["searchTerms"];
       this.searchResults = props.location.params["searchResults"].filePaths;
       this.imageClick = this.imageClick.bind(this);
       this.finish = this.finish.bind(this);
+      this.setBorder = this.setBorder.bind(this);
     }
 
     imageClick(image_src) {
-      this.setState(state => ({ showBorder: !state.showBorder }))
-      this.state.selectedImages.push(image_src);
-      console.log(image_src);
+      if (this.selectedImages.includes(image_src)){
+        const index = this.selectedImages.indexOf(image_src);
+        this.selectedImages.splice(index, 1);
+        console.log("removing");
+
+      }
+      else {
+        this.selectedImages.push(image_src);
+        console.log("pushing");
+      }
+      this.forceUpdate()
+    }
+
+    setBorder(image_src) {
+      if (this.selectedImages.includes(image_src)){
+        return CSSVariables.border;
+      }
+      else {
+        return CSSVariables.noBorder;
+      }
     }
     finish() {
       this.props.history.push({
         pathname: '/finishPage',
         params: {
-          selectedImages: this.state.selectedImages
+          selectedImages: this.selectedImages
         }
       });
     }
@@ -55,7 +74,7 @@ const CSSVariables = {
             <h5> SELECT IMAGES THAT SHOW YOUR LOGO </h5>
             <div className="box">
             {this.searchResults.map((photo) => {
-              return <div className="dataSetBox" key={photo.toString()}><img id={this.state.ID} src={photo} onClick={() => { this.imageClick(photo.toString()) }} style={this.state.showBorder ? CSSVariables.border : CSSVariables.noBorder} alt="Image" height="350" width="450" margin="100px" /></div>;
+              return <div className="dataSetBox" key={photo.toString()}><img id={this.state.ID} src={photo} onClick={() => { this.imageClick(photo.toString()) }} style={this.setBorder(photo.toString())} alt="Image" height="350" width="450" margin="100px" /></div>;
             })}
             </div>
         </center>
