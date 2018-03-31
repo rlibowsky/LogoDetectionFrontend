9ae29341a448348cb6d254cdc4116910f248ca76
rlibowsky <libowsky@usc.edu>
2018-03-31 11:56:31 -0700

    searching stuff more built out

--- logo-detection-frontend/src/components/search.js
+++ logo-detection-frontend/src/components/search.js
@@ -15,6 +15,9 @@ import { Button, Container, Form, FormGroup, Input } from 'reactstrap';
         hashtags: [],
         users: [],
         classifiers: [],
+        currentHashtag: '',
+        currentUser: '',
+        currentClassifier: '',
         data: '',
         imageJSON: [],
         ready: false,
@@ -25,24 +28,83 @@ import { Button, Container, Form, FormGroup, Input } from 'reactstrap';
         this.props.history.push('/login');
         return;
       }
+
+      // this.classifierList = {};
+      // this.userList = {};
+      // this.hashtagList = {};
+
       this.nextPage = this.nextPage.bind(this);
+      this.addUser = this.addUser.bind(this);
+      this.addClassifier = this.addClassifier.bind(this);
+      this.addHashtag = this.addHashtag.bind(this);
+      this.buildClassifierList = this.buildClassifierList.bind(this);
+      this.buildHashtagList = this.buildHashtagList.bind(this);
+      this.buildUserList = this.buildUserList.bind(this);
+
     }
     handleHashtagChange = (e) => {
       this.setState({
-        hashtag: e.target.value
+        currentHashtag: e.target.value
       })
     }
 
     handleUsersChange = (e) => {
       this.setState({
-        users: e.target.value
+        currentUser: e.target.value
       })
     }
 
     handleClassifiersChange = (e) => {
       this.setState({
-        classifiers: e.target.value
+        currentClassifier: e.target.value
       })
+    }
+
+    addUser() {
+      var userList = this.state.users;
+      userList.push(this.state.currentUser);
+      this.setState ({
+        users: userList,
+        currentUser: ''
+      })
+    }
+
+    addHashtag() {
+      var hashtagList = this.state.hashtags;
+      hashtagList.push(this.state.currentHashtag);
+      this.setState ({
+        hashtags: hashtagList,
+        currentHashtag: ''
+      })
+    }
+
+    addClassifier() {
+      var classifierList = this.state.classifiers;
+      classifierList.push(this.state.currentClassifier);
+      this.setState ({
+        classifiers: classifierList,
+        currentClassifier: ''
+      })
+      this.buildClassifierList();
+    }
+
+    buildClassifierList () {
+      console.log("building classifier list");
+      this.classifierList = this.state.classifiers.map(function(classifier){
+        console.log(classifier)
+        return(
+         <div key = {classifier.toString()}> 
+         {classifier.toString()}
+        </div>);
+      })
+    }
+
+    buildUserList () {
+      
+    }
+
+    buildHashtagList () {
+      
     }
 
     handleSubmit = (e) => {
@@ -89,43 +151,66 @@ import { Button, Container, Form, FormGroup, Input } from 'reactstrap';
       // })
       this.props.history.push('/scraperesults');
     }
+
+
   render() {
+    console.log("rendering ");
+    console.log(this.classifierList);
     return (
+      
     <Container>
         <center>
             <h2> SEARCH </h2>
             <div className="header-space"></div>
+            <h3> Currently Scraping for: </h3>
+            <div>
+              <div>
+                Hashtags:
+              </div>
+              <div>
+                Users:
+              </div>
+              <div>
+                Classifiers:
+                <ul>
+                  {this.classifierList}
+                </ul>
+              </div>
+            </div>
             <Form>
             <FormGroup> 
               <div>
               <Input 
                 type="text" 
-                id="hashtag" 
-                placeholder="Choose hashtags to scrape" 
+                id="currentHashtag" 
+                placeholder="Add hashtags to scrape" 
                 className="searchBox"
-                value={this.state.hashtag}
+                value={this.state.currentHashtag}
                 onChange={this.handleHashtagChange}
                 />
+                <Button onClick={this.Hashtag}> Add Hashtag </Button>
                 </div>
                 <div>
                 <Input 
                 type="text" 
-                id="users" 
-                placeholder="Choose users to scrape" 
+                id="currentUser" 
+                placeholder="Add users to scrape" 
                 className="searchBox"
-                value={this.state.users}
+                value={this.state.currentUser}
                 onChange={this.handleUsersChange}
                 />
+                <Button onClick={this.addUser}> Add User </Button>
                 </div>
                 <div>
                 <Input 
                 type="text" 
-                id="classifiers" 
-                placeholder="Choose classifiers to use" 
+                id="currentClassifier" 
+                placeholder="Add classifiers to use" 
                 className="searchBox"
-                value={this.state.classifiers}
+                value={this.state.currentClassifier}
                 onChange={this.handleClassifiersChange}
                 />
+                <Button onClick={this.addClassifier}> Add Classifier </Button>
                 </div>
               <Button onClick={this.handleSubmit}  className="searchBtn"> DONE </Button>
               <Loading
